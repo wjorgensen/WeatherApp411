cb16249a52cb52cbc672cfb8c078e340571545c2
Wezabis <wjorgensen29@gmail.com>
2024-12-05 20:32:04 -0500

    Added clear database for smoke tests

--- database.py
+++ database.py
@@ -26,9 +26,22 @@ def init_db():
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
 
+def clear_db():
+    db = get_db()
+    db.execute('DELETE FROM favorite_locations')
+    db.execute('DELETE FROM users')
+    db.commit()
+
 @click.command('init-db')
 @with_appcontext
 def init_db_command():
     """Clear the existing data and create new tables."""
     init_db()
-    click.echo('Initialized the database.') 
+    click.echo('Initialized the database.')
+
+@click.command('clear-db')
+@with_appcontext
+def clear_db_command():
+    """Clear all data from the database."""
+    clear_db()
+    click.echo('Cleared all data from the database.') 
